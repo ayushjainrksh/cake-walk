9d3ee4f5d09efb32a8886a4ac14446e5c52437a2
Ayush Jain <ayushjainrksh@gmail.com>
2020-09-12 16:47:50 +0530

    Add mongodb database

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const middlewares = require("./utils/middlewares");
+const mongoose = require("mongoose");
+
+try {
+  mongoose.connect("mongodb://localhost:27017/cake-walk-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  console.log("Connected to MongoDB server...");
+} catch (error) {
+  console.log("Error in connection to mongodb : ", error.message);
+}
 
 app.use(middlewares.requestLogger);
 
