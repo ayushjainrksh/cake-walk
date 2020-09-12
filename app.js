f74701201253ca9a9b7222b36ce0c53548bd9b38
Ayush Jain <ayushjainrksh@gmail.com>
2020-09-12 16:32:00 +0530

    Add request logging middleware

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+const { request } = require("express");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const middlewares = require("./utils/middlewares");
+
+app.use(middlewares.requestLogger);
 
 app.get("/ping", function (req, res) {
   res.send("pong");
 });
+
+app.use(middlewares.unknownEndpoint);
 
 app.listen(PORT, function (err) {
   if (err) console.log(err);
