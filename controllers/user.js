89291a4319e0c488fdfc02e8c048b852b839bda8
Ayush Jain <ayushjainrksh@gmail.com>
2020-09-13 11:19:16 +0530

    Add user login

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
 const User = require("../models/user");
 
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
 
 const ping = async (req, res) => {
   return res.send("pong");
@@ -36,7 +39,81 @@ const createUser = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email }).select(
+      "password"
+    );
+
+    try {
+      const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+      if (isMatch) {
+        const payload = {
+          id: user._id,
+          user,
+        };
+
+        try {
+          const token = jwt.sign(payload, process.env.JWT_ENCRYPTION, {
+            expiresIn: process.env.JWT_EXPIRATION,
+          });
+
+          console.log(`User logged in: ${req.body.email}`);
+          try {
+            const newUser = await User.findById(user._id);
+            return res.send({
+              success: true,
+              result: newUser._id,
+              token: "Bearer " + token,
+              message: "Logged In",
+            });
+          } catch (error) {
+            console.log(`Error while updating user`);
+            return res.send({
+              result: err,
+              success: false,
+              message: "Error while updating the user.",
+              token: null,
+            });
+          }
+        } catch (error) {
+          console.log(`Error while jwt signing`);
+          return res.send({
+            result: null,
+            success: false,
+            error: "Error signing token",
+          });
+        }
+      } else {
+        return res.send({
+          success: false,
+          message: "Password incorrect",
+          result: null,
+          token: null,
+        });
+      }
+    } catch (error) {
+      return res.send({
+        result: error,
+        success: false,
+        message: "Error while comparing hashes",
+        token: null,
+      });
+    }
+  } catch (error) {
+    log.info(`Can not find user with email : ${req.body.email}`);
+    return res.send({
+      result: null,
+      success: false,
+      message: "User does not exists with given email id.",
+      token: null,
+    });
+  }
+};
+
 module.exports = {
   ping,
   createUser,
+  login,
 };
--- models/user.js
+++ models/user.js
@@ -6,15 +6,22 @@ const UserSchema = new Schema(
   {
     firstName: {
       type: String,
+      trim: true,
+      required: true,
     },
     lastName: {
       type: String,
+      trim: true,
     },
     email: {
       type: String,
+      trim: true,
+      required: true,
     },
     password: {
       type: String,
+      required: true,
+      select: false,
     },
   },
   { timestamps: true }
